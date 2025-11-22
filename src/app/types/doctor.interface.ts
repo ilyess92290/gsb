bfa367dc5ba957d41979260316b54f1527a9666f
Tlati <ilyesstlati92@egmail.com>
2025-11-22 22:38:41 +0100

    corrigé doctor.interface.ts et rajouter medecin.interface.ts

--- src/app/types/doctor.interface.ts
+++ src/app/types/doctor.interface.ts
@@ -1,8 +1,8 @@
-export interface doctor {
-    id:number;
-    firstName:string;
-    lastName:string;
-    email:string;
-    speciality:string;
-    address:string;
+export interface Doctor {
+    id: number;
+    firstName: string;
+    lastName: string;
+    email: string;
+    speciality: string;
+    address: string;
 }
