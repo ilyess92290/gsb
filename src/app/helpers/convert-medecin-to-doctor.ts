651cf191945008eee2a462f52dcbf4f42e197e75
Tlati <ilyesstlati92@egmail.com>
2025-11-23 22:23:12 +0100

    feat(helpers): fix  convertMedecinToDoctor helper add speciality

--- src/app/helpers/convert-medecin-to-doctor.ts
+++ src/app/helpers/convert-medecin-to-doctor.ts
@@ -3,11 +3,12 @@ import { Doctor } from '../types/doctor.interface';
 
 // Fonction classique (pas fléchée) 
 export function convertMedecinToDoctor(medecin: Medecin): Doctor {
-  return {
+  return  {
     id: medecin.id,   
     lastName: medecin.nom,
     firstName: medecin.prenom,
     email: medecin.email,
+    speciality: medecin.specialite,
     address: medecin.adresse, 
     
   };
