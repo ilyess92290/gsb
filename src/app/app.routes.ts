388573fd8de6c66944a7a2bd37d6c3b2f60125b1
guts <guts@ATOM.home>
2025-11-24 15:27:58 +0100

    feat(pages): add DoctorsPageComponent with signal

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,11 @@
 import { Routes } from '@angular/router';
+import {Medecin} from './types/medecin.interface';
+import { Doctor } from './types/doctor.interface';
+import {DoctorsPage} from './pages/doctors-page/doctors-page'
 
-export const routes: Routes = [];
+
+export const routes: Routes = [
+  { path: '', redirectTo: '/medecins', pathMatch: 'full' },
+  { path: 'medecins', component: DoctorsPage},
+  { path: '**', redirectTo: '/medecins' },
+];
---
+++ src/app/pages/doctors-page/doctors-page.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-doctors-page',
+  imports: [],
+  templateUrl: './doctors-page.html',
+  styleUrl: './doctors-page.css',
+})
+export class DoctorsPage {
+
+}
